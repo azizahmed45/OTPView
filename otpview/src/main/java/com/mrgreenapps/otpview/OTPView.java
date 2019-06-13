7125900417420d2b796690d754213b4d28cc9f89
Aziz Ahmed <azizahmedsabuj@gmail.com>
2019-06-14 00:30:23 +0600

    initial setup of pinview ui

--- otpview/src/main/java/com/mrgreenapps/otpview/OTPView.java
+++ otpview/src/main/java/com/mrgreenapps/otpview/OTPView.java
@@ -2,13 +2,16 @@ package com.mrgreenapps.otpview;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
-import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Rect;
 import android.text.InputFilter;
 import android.text.InputType;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -16,6 +19,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OTPView extends LinearLayout {
+
+    private static final String TAG = "OTPView";
 
     Context context;
 
@@ -31,10 +36,17 @@ public class OTPView extends LinearLayout {
     int mBoxWidth;
     int mBoxHeight;
 
+    int mBoxHorizontalPadding;
+
+    int mGap;
+
     int mTextSize;
 
 
-    List<EditText> pinBoxList;
+    List<TextView> pinBoxList;
+
+    EditText mInput;
+
 
     public OTPView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -51,48 +63,53 @@ public class OTPView extends LinearLayout {
 
             mTextColor = typedArray.getColorStateList(R.styleable.OTPView_otp_text_color);
 
-            mBoxWidth = typedArray.getDimensionPixelSize(R.styleable.OTPView_otp_box_width, getResources().getDimensionPixelSize(R.dimen.box_default_width));
-            mBoxHeight = typedArray.getDimensionPixelSize(R.styleable.OTPView_otp_box_height, getResources().getDimensionPixelSize(R.dimen.box_default_height));
+            mBoxWidth = typedArray.getDimensionPixelSize(R.styleable.OTPView_otp_box_width, LayoutParams.WRAP_CONTENT);
+            mBoxHeight = typedArray.getDimensionPixelSize(R.styleable.OTPView_otp_box_height, LayoutParams.WRAP_CONTENT);
+
+            mBoxHorizontalPadding = typedArray.getDimensionPixelSize(R.styleable.OTPView_otp_box_horizontal_padding, getResources().getDimensionPixelSize(R.dimen.default_box_horizontal_padding));
 
             mTextSize = typedArray.getDimensionPixelSize(R.styleable.OTPView_otp_text_size, getResources().getDimensionPixelSize(R.dimen.default_text_size));
+
+            mGap = typedArray.getDimensionPixelSize(R.styleable.OTPView_otp_box_gap, getResources().getDimensionPixelSize(R.dimen.default_box_gap));
 
         } finally {
             typedArray.recycle();
         }
 
-        setWeightSum(mOtpLength);
-
         pinBoxList = new ArrayList<>();
         for(int i = 0; i < mOtpLength; i++){
 
-            EditText pinBox = new EditText(context);
+            TextView pinBox = new TextView(context);
             pinBox.setText(String.valueOf(i));
 
             pinBox.setTextSize(mTextSize);
-            pinBox.setTextColor(mTextColor.getDefaultColor());
+
+            if(mTextColor != null) pinBox.setTextColor(mTextColor.getDefaultColor());
 
             pinBox.setInputType(InputType.TYPE_CLASS_NUMBER);
 
             pinBox.setFilters(new InputFilter[] {new InputFilter.LengthFilter(1)});
 
-            pinBox.setLayoutParams(new LayoutParams(mBoxWidth, mBoxHeight));
 
 
-            pinBox.setBackgroundColor(mBoxDefaultColor.getDefaultColor());
+
+            LayoutParams p = new LayoutParams(mBoxWidth, mBoxHeight);
+
+            p.setMargins(mGap, 0, mGap, 0);
+
+            pinBox.setLayoutParams(p);
+            pinBox.setGravity(Gravity.CENTER);
+
+            pinBox.setPadding(mBoxHorizontalPadding, 0, mBoxHorizontalPadding, 0);
+
+
+            if(mBoxDefaultColor != null) pinBox.setBackgroundColor(mBoxDefaultColor.getDefaultColor());
 
             pinBoxList.add(pinBox);
 
             addView(pinBox);
         }
 
-
-
-//        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-//
-//        TextView textView = (TextView) inflater.inflate(R.layout.text_view, null, false);
-//        textView.setTextSize(55);
-//
-//        addView(textView);
 
     }
 
